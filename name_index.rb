d1496f9e9d9fe7c68f4f0093de95aabcd4937a0b
Yuka Saito <yucatio.com@gmail.com>
2019-05-19 23:03:22 +0900

    Changed to my answer

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,21 @@
 class NameIndex
+  NAME_INDEX_HASH = {
+      'ア' => %w(ア イ ウ エ オ ヴ),
+      'カ' => %w(カ キ ク ケ コ ガ ギ グ ゲ ゴ),
+      'サ' => %w(サ シ ス セ ソ ザ ジ ズ ゼ ゾ),
+      'タ' => %w(タ チ ツ テ ト ダ ヂ ヅ デ ド),
+      'ナ' => %w(ナ ニ ヌ ネ ノ),
+      'ハ' => %w(ハ ヒ フ ヘ ホ バ ビ ブ ベ ボ パ ピ プ ペ ポ),
+      'マ' => %w(マ ミ ム メ モ),
+      'ヤ' => %w(ヤ ユ ヨ),
+      'ラ' => %w(ラ リ ル レ ロ),
+      'ワ' => %w(ワ ヲ ン)
+  }.freeze
+
   def self.create_index(names)
-    []
+    names.sort.group_by {|name|
+      index_kana = NAME_INDEX_HASH.find(['Others']) {|_, value| value.include?(name[0])}
+      index_kana[0]
+    }.to_a
   end
 end
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index([])).to eq [] }
+  describe '#create_index' do
+    specify {expect(NameIndex.create_index([])).to eq []}
+    specify {expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq [['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']]]}
+    specify {expect(NameIndex.create_index(['サトウ', 'スズキ', 'タカハシ', 'イケガミ', 'アラキ', 'デグチ', 'ヌマタ'])).to eq [['ア', ['アラキ', 'イケガミ']], ['サ', ['サトウ', 'スズキ']], ['タ', ['タカハシ', 'デグチ']], ['ナ', ['ヌマタ']]]}
+  end
 end
